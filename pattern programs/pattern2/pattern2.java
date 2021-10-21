a1d4e52a6c9a9f33e2698c2f43b461f2d338b171
KARTHI2710 <92161117+KARTHI2710@users.noreply.github.com>
2021-10-21 17:47:44 +0800

    floyd triangle pattern

---
+++ pattern programs/pattern2/pattern2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class pattern2{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+System.out.println("enter n value");
+int n=sc.nextInt();
+for(int i=1;i<=n;i++){
+for(int j=i;j<=n;j++){
+System.out.print("* ");
+}
+System.out.println(" ");
+}
+}
+}
