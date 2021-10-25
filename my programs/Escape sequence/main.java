cba064c2593160ec6a2c4b872989ba4eecb36047
KARTHI2710 <92161117+KARTHI2710@users.noreply.github.com>
2021-10-25 16:16:54 +0800

    Escape Sequence program

---
+++ my programs/Escape sequence/main.java
@@ -0,0 +1,10 @@
+//Escape Sequence
+class main{
+public static void main(String args[]){
+System.out.println("Hello\njava");
+System.out.println("Hello\tjava");
+System.out.println("Hello \'java\'");
+System.out.println("Hello \"java\"");
+System.out.println("Hello \\java");
+}
+}
